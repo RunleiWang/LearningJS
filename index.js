4d251de0727a73e6f2cad7e1ff662971e465b18d
Runlei Wang <rlwang@CNrlwang.local>
2018-12-20 17:50:00 +0800

    first sight at react

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-const server = require('./server')
-server.listen(3000)
+//const server = require('./server')
+//server.listen(3000)
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//component
+function RL(){
+    return <div>
+            <ul id="ul">
+                <li className="item">runlei</li>
+                <li className="item">runlei</li>
+                <li className="item">runlei</li>
+                <li className="item">runlei</li>
+            </ul>
+            <p>Runlei Wang</p>
+        <RN/>
+    </div>
+
+}
+
+function RN(){
+    return  <p>llll</p>
+
+}
+
+ReactDOM.render(
+    <RL />, document.getElementById('root')
+)
